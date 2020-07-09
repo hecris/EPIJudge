93e9f621319804af11b2993242d2084509933222
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-09 18:08:26 -0400

    arrays: add problem 5.7 (buy and sell stock II)

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -3,9 +3,27 @@ from typing import List
 from test_framework import generic_test
 
 
+# NOTE: consider iterating backwards to solve array problems
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    before = []
+    msf, profit = float('inf'), 0
+    for i in range(len(prices)):
+        profit = max(profit, prices[i] - msf)
+        before.append(profit)
+        msf = min(msf, prices[i])
+
+    msf, profit = float('-inf'), 0
+    after = [0] * len(prices)
+    for i in reversed(range(len(prices))):
+        profit = max(profit, msf - prices[i])
+        after[i] = profit
+        msf = max(msf, prices[i])
+
+    res = 0
+    for i in range(len(prices)):
+        res = max(res, (before[i-1] if i > 0 else 0) + after[i])
+
+    return res
 
 
 if __name__ == '__main__':
