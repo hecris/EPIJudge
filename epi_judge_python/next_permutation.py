1fa6a16cfaaed9e5f8cff074c2068b10edd71841
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-10 20:58:33 -0400

    arrays: add problem 5.11 (next permutation)

--- epi_judge_python/next_permutation.py
+++ epi_judge_python/next_permutation.py
@@ -3,9 +3,28 @@ from typing import List
 from test_framework import generic_test
 
 
+def reverse(l, lo, hi):
+    while lo < hi:
+        l[lo], l[hi] = l[hi], l[lo]
+        lo += 1
+        hi -= 1
+
+# NOTE: I found this pretty hard
 def next_permutation(perm: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    prefix_end = len(perm) - 2
+    while prefix_end >= 0 and perm[prefix_end] >= perm[prefix_end + 1]:
+        prefix_end -= 1
+
+    if prefix_end == -1:
+        return []
+
+    smallest = len(perm) - 1
+    while perm[smallest] <= perm[prefix_end]:
+        smallest -= 1
+
+    perm[smallest], perm[prefix_end] = perm[prefix_end], perm[smallest]
+    reverse(perm, prefix_end+1, len(perm)-1)
+    return perm
 
 
 if __name__ == '__main__':
