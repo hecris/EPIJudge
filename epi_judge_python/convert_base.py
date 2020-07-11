84c50f41d27e11212c00d227502b77e78a356890
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-11 19:22:20 -0400

    strings: add problem 6.2 (base conversion)

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
+import string
+import functools
 
 
 def convert_base(num_as_string: str, b1: int, b2: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    def convert_to_s(num_as_int, base):
+        res = []
+        while True:
+            digit = num_as_int % base
+            num_as_int //= base
+            res.append(string.hexdigits[digit].upper())
+            if num_as_int == 0:
+                break
+        res.reverse()
+        return ''.join(res)
+
+
+    negative = num_as_string[0] == '-'
+    num_as_int = functools.reduce(
+            lambda running_sum, c: running_sum * b1 + string.hexdigits.index(c.lower()),
+            num_as_string[negative:], 0)
+    return ('-' if negative else '') + convert_to_s(num_as_int, b2)
 
 
 if __name__ == '__main__':
