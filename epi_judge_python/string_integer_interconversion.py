9d22dd613935cf8db2683ec05e14dcc1f65afa5b
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-11 18:39:34 -0400

    strings: add problem 6.1 (string <-> int conversion)

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,13 +3,39 @@ from test_framework.test_failure import TestFailure
 
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    res = []
+    negative = False
+    if x < 0:
+        negative = True
+        x *= -1
+
+    while True:
+        digit = x % 10
+        x //= 10
+        res.append(chr(digit + 48))
+        if x == 0:
+            break
+
+    if negative:
+        res.append('-')
+
+    res.reverse()
+    return ''.join(res)
 
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    res = 0
+    sign = 1
+    for i in (range(len(s))):
+        if s[i] == '-':
+            sign = -1
+            continue
+        if s[i] == '+':
+            continue
+        digit = ord(s[i]) - 48
+        res *= 10
+        res += digit
+    return res * sign
 
 
 def wrapper(x, s):
@@ -20,6 +46,8 @@ def wrapper(x, s):
 
 
 if __name__ == '__main__':
+    # print(string_to_int("0"))
+    # print(int_to_string(0))
     exit(
         generic_test.generic_test_main('string_integer_interconversion.py',
                                        'string_integer_interconversion.tsv',
