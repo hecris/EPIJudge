79959361b47b2d5899dcd935a09d58d88062460e
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-11 16:43:12 -0400

    arrays: add problems 5.19 (rotate matrix) and 5.20 (pascal triangle)

--- epi_judge_python/matrix_rotation.py
+++ epi_judge_python/matrix_rotation.py
@@ -4,7 +4,42 @@ from test_framework import generic_test
 
 
 def rotate_matrix(square_matrix: List[List[int]]) -> None:
-    # TODO - you fill in here.
+    def rotate_number(x, y, lo, hi, k):
+        tmp = square_matrix[x][y]
+        y += k
+        while y > hi:
+            y -= 1
+            x += 1
+
+        tmp, square_matrix[x][y] = square_matrix[x][y], tmp
+
+        x += k
+        while x > hi:
+            x -= 1
+            y -= 1
+
+        tmp, square_matrix[x][y] = square_matrix[x][y], tmp
+
+        y -= k
+        while y < lo:
+            y += 1
+            x -= 1
+
+        tmp, square_matrix[x][y] = square_matrix[x][y], tmp
+
+        x -= k
+        while x < lo:
+            x += 1
+            y += 1
+
+        tmp, square_matrix[x][y] = square_matrix[x][y], tmp
+
+    lo, hi = 0, len(square_matrix) - 1
+    while lo < hi:
+        for i in range(lo, hi):
+            rotate_number(lo, i, lo, hi, hi - lo)
+        lo += 1
+        hi -= 1
     return
 
 
