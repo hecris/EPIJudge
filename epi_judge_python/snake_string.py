6993f26f527c0848e93d226a548223c8d2613b0c
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-12 12:27:01 -0400

    strings: add problem 6.10 (snake string)

--- epi_judge_python/snake_string.py
+++ epi_judge_python/snake_string.py
@@ -3,7 +3,17 @@ from test_framework import generic_test
 
 def snake_string(s: str) -> str:
     # TODO - you fill in here.
-    return ''
+    res = []
+    for i in range(1, len(s), 4):
+        res.append(s[i])
+
+    for i in range(0, len(s), 2):
+        res.append(s[i])
+
+    for i in range(3, len(s), 4):
+        res.append(s[i])
+
+    return ''.join(res)
 
 
 if __name__ == '__main__':
