e63ffcbb92b983e6f47d1d263cecf862c3d27274
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-11 21:37:53 -0400

    strings: add problems 6.5 (palindromic sentence) and 6.6 (reverse words)

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -2,7 +2,23 @@ from test_framework import generic_test
 
 
 def is_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
+    lo, hi = 0, len(s) - 1
+    while True:
+        while lo < len(s) and not s[lo].isalnum():
+            lo += 1
+
+        while hi >= 0 and not s[hi].isalnum():
+            hi -= 1
+
+        if lo >= hi:
+            break
+
+        if s[lo].lower() != s[hi].lower():
+            return False
+
+        lo += 1
+        hi -= 1
+
     return True
 
 
