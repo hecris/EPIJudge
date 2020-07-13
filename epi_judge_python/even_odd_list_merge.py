fb0042600ae695ebad514bb9d7ae4f9141da1cc2
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-13 11:31:23 -0400

    linked lists: add problem 7.10 (even odd merge)

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -5,8 +5,23 @@ from test_framework import generic_test
 
 
 def even_odd_merge(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    even = even_tail = ListNode(0)
+    odd = odd_tail = ListNode(0)
+
+    cur, k = L, 0
+    while cur:
+        tmp = cur.next
+        if k % 2:
+            cur.next, odd_tail.next = None, cur
+            odd_tail = odd_tail.next
+        else:
+            cur.next, even_tail.next = None, cur
+            even_tail = even_tail.next
+        cur = tmp
+        k += 1
+
+    even_tail.next = odd.next
+    return even.next
 
 
 if __name__ == '__main__':
