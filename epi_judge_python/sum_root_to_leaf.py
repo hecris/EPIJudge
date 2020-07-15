b4d3c1d00809f04812298dd888f4ab188505daff
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-14 20:56:33 -0400

    binary trees: add problem 9.5 (sum root to leaf)

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -2,9 +2,24 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+# NOTE: there is a way to do this without nonlocal
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
-    # TODO - you fill in here.
-    return 0
+    res = 0
+    def preorder(root, x):
+        nonlocal res
+        if not root:
+            return
+
+        x = (x << 1) + root.data
+        if not root.left and not root.right:
+            res += x
+            return
+
+        preorder(root.left, x)
+        preorder(root.right, x)
+
+    preorder(tree, 0)
+    return res
 
 
 if __name__ == '__main__':
