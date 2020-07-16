055bbed9f7288eb63e3e0e23137ef7691dfa6062
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-15 21:47:12 -0400

    binary trees: add problem 9.11 (tree from inorder and preorder)

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -6,8 +6,21 @@ from test_framework import generic_test
 
 def binary_tree_from_preorder_inorder(preorder: List[int],
                                       inorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    indices = {x: i for i, x in enumerate(inorder)}
+
+    def helper(plo, phi, ilo, ihi):
+        if plo >= phi or ilo >= ihi:
+            return None
+
+        root_val = preorder[plo]
+        root_index = indices[root_val]
+        left_size = root_index - ilo
+        root = BinaryTreeNode(root_val)
+        root.left = helper(plo+1, plo+1+left_size, ilo, root_index)
+        root.right = helper(plo+1+left_size, phi, root_index+1, ihi)
+        return root
+
+    return helper(0, len(preorder), 0, len(inorder))
 
 
 if __name__ == '__main__':
--- problem_mapping.js
+++ problem_mapping.js
@@ -1150,7 +1150,7 @@ problem_mapping = {
                 "total": 3852
             },
             "Python: tree_from_preorder_inorder.py": {
-                "passed": 0,
+                "passed": 3852,
                 "total": 3852
             }
         },
@@ -1192,7 +1192,7 @@ problem_mapping = {
                 "total": 3852
             },
             "Python: tree_exterior.py": {
-                "passed": 0,
+                "passed": 1,
                 "total": 3852
             }
         },
