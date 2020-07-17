96aa09c786f026a946e5884136ec9e984d17e024
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-16 22:30:58 -0400

    heaps: add problem 10.1 (merge k sorted arrays)

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,25 @@
 from typing import List
 
 from test_framework import generic_test
+import heapq
 
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    iterators = [iter(l) for l in sorted_arrays]
+    heap = [(next(iterator), i) for i, iterator in enumerate(iterators)]
+    heapq.heapify(heap)
+
+    res = []
+
+    while heap:
+        val, itr_idx = heapq.heappop(heap)
+        res.append(val)
+        itr = iterators[itr_idx]
+        next_val = next(itr, None)
+        if next_val is not None:
+            heapq.heappush(heap, (next_val, itr_idx))
+
+    return res
 
 
 if __name__ == '__main__':
