afa3948980cdb036bfdc6f1ee6b43d37510dc77f
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-17 17:26:16 -0400

    heaps: add problem 10.3 (sort k-sorted array)

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -1,12 +1,24 @@
 from typing import Iterator, List
 
 from test_framework import generic_test
+import heapq
 
 
 def sort_approximately_sorted_array(sequence: Iterator[int],
                                     k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    res = []
+    heap = [next(sequence) for _ in range(k)]
+    heapq.heapify(heap)
+
+    val = next(sequence, None)
+    while heap:
+        if val is not None:
+            res.append(heapq.heappushpop(heap, val))
+            val = next(sequence, None)
+        else:
+            res.append(heapq.heappop(heap))
+
+    return res
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):
