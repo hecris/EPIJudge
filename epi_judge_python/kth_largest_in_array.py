3bfb717ae0516af61bd804dbd6d2fb681f72024d
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-22 13:08:46 -0400

    minor comment

--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -8,6 +8,7 @@ import random
 # find_kth_largest(1, A) returns 3, find_kth_largest(2, A) returns 2,
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
 def find_kth_largest(k: int, A: List[int]) -> int:
+    # NOTE: this is quickselect with O(n) avg time
     def partition(A, lo, hi, pivot):
         A[lo], A[pivot] = A[pivot], A[lo]
         i, j = lo, hi
