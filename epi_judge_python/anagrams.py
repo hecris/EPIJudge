b7e05bb62185dcbdeb749c062dc115e9d479fdaf
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-22 18:59:23 -0400

    hash tables: add problem 12.0 (group anagrams)

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -1,11 +1,23 @@
 from typing import List
 
 from test_framework import generic_test, test_utils
+from collections import defaultdict
 
 
 def find_anagrams(dictionary: List[str]) -> List[List[str]]:
-    # TODO - you fill in here.
-    return []
+    def get_counter(word):
+        counter = [0] * 26
+        for char in word:
+            if char == ' ':
+                continue
+            counter[ord(char) - ord('a')] += 1
+        return tuple(counter)
+
+    d = defaultdict(list)
+    for word in dictionary:
+        d[get_counter(word)].append(word)
+
+    return [group for group in d.values() if len(group) > 1]
 
 
 if __name__ == '__main__':
