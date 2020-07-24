737c338ae63a24d0116e0da482e2e87218d89a5e
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-24 17:50:40 -0400

    hash tables: add problem 12.7 (minimum window subsequence)

--- epi_judge_python/smallest_subarray_covering_all_values.py
+++ epi_judge_python/smallest_subarray_covering_all_values.py
@@ -8,11 +8,33 @@ from test_framework.test_utils import enable_executor_hook
 Subarray = collections.namedtuple('Subarray', ('start', 'end'))
 
 
+# NOTE: struggled with this
 def find_smallest_sequentially_covering_subset(paragraph: List[str],
                                                keywords: List[str]
                                                ) -> Subarray:
-    # TODO - you fill in here.
-    return Subarray(0, 0)
+    keyword_idx = {x: i for i, x in enumerate(keywords)}
+    last_occur = [-1] * len(keywords)
+    shortest_sub = [float('inf')] * len(keywords)
+
+    ans = None
+    for i, word in enumerate(paragraph):
+        if word not in keyword_idx:
+            continue
+
+        idx = keyword_idx[word]
+        if idx == 0:
+            shortest_sub[idx] = 1
+        elif shortest_sub[idx - 1] != float('inf'):
+            distance = i - last_occur[idx - 1]
+            shortest_sub[idx] = shortest_sub[idx - 1] + distance
+
+        last_occur[idx] = i
+
+        if idx == len(keywords) - 1:
+            if not ans or shortest_sub[-1] < ans.end - ans.start + 1:
+                ans = Subarray(i - shortest_sub[-1] + 1, i)
+
+    return ans
 
 
 @enable_executor_hook
