8d814fac0d76ad7dec378e9cfd5a302dea639418
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-24 18:22:49 -0400

    sorting: add alternate (but less optimal solution) for problem 13.1 (intersection of two sorted arrays)

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -4,6 +4,14 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
+    # NOTE: cool Python one-liner to get distinct elements and demonstration
+    # of bisect
+    # import bisect
+    # def search(arr, target):
+    #     k = bisect.bisect_left(arr, target)
+    #     return k < len(arr) and arr[k] == target
+
+    # return [a for i, a in enumerate(A) if (i == 0 or A[i-1] != a) and search(B, a)]
     res = []
     i = j = 0
     while True:
