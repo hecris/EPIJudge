356e1898b342d1d4ef1c62994a56f912331a56c3
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-24 19:04:59 -0400

    sorting: add problem 13.2 (merge two sorted arrays)

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -5,7 +5,22 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
+    i, j = m - 1, n - 1
+    write = m + n - 1
+
+    while write >= 0:
+        num1 = A[i] if i >= 0 else float('-inf')
+        num2 = B[j] if j >= 0 else float('-inf')
+
+        if num1 > num2:
+            A[write] = num1
+            i -= 1
+        else:
+            A[write] = num2
+            j -= 1
+
+        write -= 1
+
     return
 
 
