a02e13dd4c288e0d1aaffc476731ec6b17ee6f78
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-24 09:44:35 -0400

    hash tables: add problem 12.10 (string decompositions)

--- epi_judge_python/string_decompositions_into_dictionary_words.py
+++ epi_judge_python/string_decompositions_into_dictionary_words.py
@@ -1,11 +1,47 @@
 from typing import List
 
 from test_framework import generic_test
+from collections import Counter
 
 
 def find_all_substrings(s: str, words: List[str]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    w = Counter(words)
+    k = len(words[0])
+    target_length = len(words) * k
+
+    res = []
+    def helper(lo):
+        word_counter = w.copy()
+        hi = lo
+        while hi < len(s):
+            next_word = s[hi:hi+k]
+            if next_word not in word_counter:
+                hi += k
+                while lo < hi:
+                    lo_word = s[lo:lo+k]
+                    if lo_word in word_counter:
+                        word_counter[lo_word] += 1
+                    lo += k
+            elif word_counter[next_word] == 0:
+                while word_counter[next_word] == 0:
+                    lo_word = s[lo:lo+k]
+                    if lo_word in word_counter:
+                        word_counter[lo_word] += 1
+                    lo += k
+            elif hi + k - lo == target_length:
+                res.append(lo)
+                lo_word = s[lo:lo+k]
+                word_counter[lo_word] += 1
+                lo += k
+            else:
+                word_counter[next_word] -= 1
+                hi += k
+
+    for i in range(k):
+        helper(i)
+
+    return sorted(res)
+
 
 
 if __name__ == '__main__':
