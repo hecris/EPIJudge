ed89268ceba553e6874481e48b8799a2045eacdf
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-24 10:19:45 -0400

    hash tables: add problem 12.11 (collatz checker)

--- epi_judge_python/collatz_checker.py
+++ epi_judge_python/collatz_checker.py
@@ -2,8 +2,24 @@ from test_framework import generic_test
 
 
 def test_collatz_conjecture(n: int) -> bool:
-    # TODO - you fill in here.
-    return False
+    verified = set()
+    for i in range(3,n+1,2):
+        seen = set()
+        cur = i
+        while cur >= i:
+            if cur in seen:
+                return False
+            seen.add(cur)
+
+            if cur % 2:
+                if cur in verified:
+                    break
+                verified.add(cur)
+                cur = (cur * 3) + 1
+            else:
+                cur //= 2
+
+    return True
 
 
 if __name__ == '__main__':
