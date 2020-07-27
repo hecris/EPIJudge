eacc0f79d83b0b7679c41940b877d2e659b01118
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-27 00:51:38 -0400

    sorting: add problem 13.12 (salary cap)

--- epi_judge_python/find_salary_threshold.py
+++ epi_judge_python/find_salary_threshold.py
@@ -4,8 +4,24 @@ from test_framework import generic_test
 
 
 def find_salary_cap(target_payroll: int, current_salaries: List[int]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    current_salaries.sort()
+    n = len(current_salaries)
+    pre = [0]
+    for i in range(1, n):
+        pre.append(pre[i-1] + current_salaries[i-1])
+
+    lo, hi = 0, n - 1
+    while lo < hi:
+        mid = (lo + hi) >> 1
+        remain = n - mid
+        payroll = pre[mid] + current_salaries[mid] * remain
+        if payroll < target_payroll:
+            lo = mid + 1
+        else:
+            hi = mid
+
+    ans = (target_payroll - pre[hi]) / (n - hi)
+    return -1 if ans > current_salaries[-1] else ans
 
 
 if __name__ == '__main__':
