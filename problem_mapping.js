bc04ff7d18ea0fa9462673a0c2522b7e286a652c
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-27 10:06:01 -0400

    BSTs: add problem 14.0 (search in bst)

--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -1790,7 +1790,7 @@ problem_mapping = {
                 "total": 955
             },
             "Python: search_in_bst.py": {
-                "passed": 0,
+                "passed": 955,
                 "total": 955
             }
         },
