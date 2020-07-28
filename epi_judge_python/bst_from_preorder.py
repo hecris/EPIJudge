35c157546d38a0277dec94a3b8a8328d3805639a
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-27 21:56:15 -0400

    BSTs: add problem 14.5 (BST from preorder)

--- epi_judge_python/bst_from_preorder.py
+++ epi_judge_python/bst_from_preorder.py
@@ -6,8 +6,22 @@ from test_framework import generic_test
 
 def rebuild_bst_from_preorder(preorder_sequence: List[int]
                               ) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+
+    def solution(lo, hi, start, end):
+        if lo > hi:
+            return None, 0
+
+        root_val = preorder_sequence[lo]
+        if not (start <= root_val <= end):
+            return None, 0
+
+        root = BstNode(root_val)
+        root.left, left_size = solution(lo+1, hi, start, root_val)
+        root.right, right_size = solution(lo+left_size+1, hi, root_val, end)
+        return root, left_size + right_size + 1
+
+
+    return solution(0, len(preorder_sequence) - 1, float('-inf'), float('inf'))[0]
 
 
 if __name__ == '__main__':
