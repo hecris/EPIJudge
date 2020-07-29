7b95a09b6e153d2f998c8cd00aa67fd337111413
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-29 17:48:58 -0400

    recursion: add iterative 15.5 (power set)

--- epi_judge_python/power_set.py
+++ epi_judge_python/power_set.py
@@ -1,9 +1,26 @@
 from typing import List
 
 from test_framework import generic_test, test_utils
+import math
 
 
 def generate_power_set(input_set: List[int]) -> List[List[int]]:
+    # NOTE: use bijections between integers, bit arrays of length n, and power set of a set of length n
+    def iterative(input_set):
+        power_set = []
+        for i in range(1 << len(input_set)):
+            subset = []
+            while i:
+                leftmost_set_bit = i & ~(i - 1)
+                index = int(math.log2(leftmost_set_bit))
+                subset.append(input_set[index])
+                i &= i - 1
+            power_set.append(subset)
+
+        return power_set
+
+    return iterative(input_set)
+
     res = []
     def solution(lo, path):
         res.append(path.copy())
