7f14e131a714dd465d61442cd3c84e5dad5cad98
hecris <christopher.he33@myhunter.cuny.edu>
2020-07-29 12:03:06 -0400

    BSTs: add problem 14.11 (design add credits)

--- epi_judge_python/adding_credits.py
+++ epi_judge_python/adding_credits.py
@@ -1,27 +1,38 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
+import bintrees
 
 
 class ClientsCreditsInfo:
+    def __init__(self):
+        self.inc = 0
+        self.client_to_credit = {}
+        self.credits_to_client = bintrees.RBTree()
+
     def insert(self, client_id: str, c: int) -> None:
-        # TODO - you fill in here.
-        return
+        self.client_to_credit[client_id] = c - self.inc
+        self.credits_to_client.setdefault(c - self.inc, set())
+        self.credits_to_client[c - self.inc].add(client_id)
 
     def remove(self, client_id: str) -> bool:
-        # TODO - you fill in here.
+        if client_id not in self.client_to_credit:
+            return False
+        credit = self.client_to_credit.pop(client_id)
+        self.credits_to_client[credit].remove(client_id)
+        if not self.credits_to_client[credit]:
+            self.credits_to_client.pop(credit)
         return True
 
     def lookup(self, client_id: str) -> int:
-        # TODO - you fill in here.
-        return 0
+        if client_id not in self.client_to_credit:
+            return -1
+        return self.client_to_credit[client_id] + self.inc
 
     def add_all(self, C: int) -> None:
-        # TODO - you fill in here.
-        return
+        self.inc += C
 
     def max(self) -> str:
-        # TODO - you fill in here.
-        return ''
+        return self.credits_to_client.max_item()[1][0]
 
 
 def client_credits_info_tester(ops):
