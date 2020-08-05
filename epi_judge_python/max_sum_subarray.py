93b42cafcb2b01d46886913dda30525b1eeac17b
hecris <christopher.he33@myhunter.cuny.edu>
2020-08-05 14:06:19 -0400

    dp: add problem 16.00 (max sum subarray)

--- epi_judge_python/max_sum_subarray.py
+++ epi_judge_python/max_sum_subarray.py
@@ -4,8 +4,13 @@ from test_framework import generic_test
 
 
 def find_maximum_subarray(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return -1
+    dp = [0] * len(A)
+    for i in range(len(A)):
+        if i == 0:
+            dp[i] = max(0, A[0])
+        else:
+            dp[i] = max(A[i], dp[i-1] + A[i])
+    return max(dp) if dp else 0
 
 
 if __name__ == '__main__':
