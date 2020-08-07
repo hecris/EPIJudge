0d7bf92a9a915ce9152906354248ad475e78575e
hecris <christopher.he33@myhunter.cuny.edu>
2020-08-07 17:47:07 -0400

    dp: add problem 16.8 (min path in triangle)

--- epi_judge_python/minimum_weight_path_in_a_triangle.py
+++ epi_judge_python/minimum_weight_path_in_a_triangle.py
@@ -4,8 +4,21 @@ from test_framework import generic_test
 
 
 def minimum_path_weight(triangle: List[List[int]]) -> int:
-    # TODO - you fill in here.
-    return 0
+    if not triangle: return 0
+
+    n = len(triangle)
+    # NOTE: can be optimized by only storing most recent row
+    dp = [[0] * (i+1) for i in range(n)]
+    dp[0][0] = triangle[0][0]
+
+    for i in range(1, n):
+        for j in range(i+1):
+            dp[i][j] = triangle[i][j] + min(
+                    dp[i-1][j] if j != i else float('inf'),
+                    dp[i-1][j-1] if j >= 1 else float('inf')
+                    )
+
+    return min(dp[n-1])
 
 
 if __name__ == '__main__':
