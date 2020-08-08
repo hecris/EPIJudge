2e1c307f6c7e47fd7d7e83ff0650c6386b1fad23
hecris <christopher.he33@myhunter.cuny.edu>
2020-08-07 21:56:22 -0400

    dp: add problem 16.10 (climb stairs)

--- epi_judge_python/number_of_traversals_staircase.py
+++ epi_judge_python/number_of_traversals_staircase.py
@@ -2,8 +2,13 @@ from test_framework import generic_test
 
 
 def number_of_ways_to_top(top: int, maximum_step: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    dp = [0] * (top + 1)
+    dp[0] = 1
+    for i in range(1, top+1):
+        for j in range(1, maximum_step+1):
+            if i - j >= 0:
+                dp[i] += dp[i-j]
+    return dp[top]
 
 
 if __name__ == '__main__':
