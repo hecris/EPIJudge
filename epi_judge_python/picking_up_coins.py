23040e5b9430cbd6d4957d7238e640037b25411b
hecris <christopher.he33@myhunter.cuny.edu>
2020-08-09 16:17:50 -0400

    dp: add problem 16.9 (picking up coins)

--- epi_judge_python/picking_up_coins.py
+++ epi_judge_python/picking_up_coins.py
@@ -4,9 +4,24 @@ from test_framework import generic_test
 
 
 def maximum_revenue(coins: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    if len(coins) == 1: return coins[0]
+    memo = {}
+    def solution(i, j, mysum):
+        if i > j:
+            return 0
 
+        if (i, j) not in memo:
+            first = solution(i+1, j, mysum - coins[i])
+            last = solution(i, j-1, mysum - coins[j])
+
+            ans = mysum - last
+            if first < last:
+                ans = mysum - first
+            memo[i,j] = ans
+
+        return memo[i, j]
+
+    return solution(0, len(coins) - 1, sum(coins))
 
 if __name__ == '__main__':
     exit(
