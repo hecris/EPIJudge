70ef2cbd38e248efd55e55407fe7f00308428632
hecris <christopher.he33@myhunter.cuny.edu>
2020-08-12 21:30:22 -0400

    graphs: add problem 18.2 (flood fill)

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -5,6 +5,18 @@ from test_framework import generic_test
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
     # TODO - you fill in here.
+    r, c = len(image), len(image[0])
+    def valid(x,y):
+        return x >= 0 and x < r and y >= 0 and y < c
+
+    color = image[x][y]
+    def dfs(x,y):
+        if not valid(x,y) or image[x][y] != color:
+            return
+        image[x][y] = not color
+        for x2, y2 in [(0,1),(1,0),(-1,0),(0,-1)]:
+            dfs(x+x2, y+y2)
+    dfs(x,y)
     return
 
 
