f9556d156c62efeab84b18b9c4d10fa464a7d274
hecris <christopher.he33@myhunter.cuny.edu>
2020-08-19 15:59:50 -0400

    primitives: add problem 4.2 (swap bits)

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+    i_mask = 1 << i
+    j_mask = 1 << j
+    i_bit = x & i_mask
+    j_bit = x & j_mask
+
+    if (i_bit or j_bit) and (not i_bit or not j_bit):
+        x ^= i_mask
+        x ^= j_mask
+
+    return x
 
 
 if __name__ == '__main__':
