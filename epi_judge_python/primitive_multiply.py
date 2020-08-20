9d510740ce7cec9252af03323f99408e82ecb9f5
hecris <christopher.he33@myhunter.cuny.edu>
2020-08-19 23:12:23 -0400

    primitives: add problem 4.5 (primitive multiplication)

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -2,8 +2,22 @@ from test_framework import generic_test
 
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    def add(a,b):
+        while b:
+            carry = a & b
+            a ^= b
+            b = carry << 1
+        return a
+
+    ans = 0
+    i = 0
+    while y:
+        if y & 1:
+            ans = add(ans, x << i)
+        # alternatively, x <<= 1
+        i += 1
+        y >>= 1
+    return ans
 
 
 if __name__ == '__main__':
