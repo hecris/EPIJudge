a4ce03493853abe40f0abf074aa0e0dd54dd6ca9
hecris <christopher.he33@myhunter.cuny.edu>
2020-08-19 20:32:49 -0400

    primitives: add problem 4.4 (closest int w/ same weight)

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def closest_int_same_bit_count(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    for i in range(63):
+        this_mask = (1 << i)
+        next_mask = (1 << (i+1))
+        if (x & this_mask and x & next_mask) or not (x & this_mask or x & next_mask):
+            continue
+
+        x ^= this_mask | next_mask
+        return x
+
+    raise ValueError('All bits are 0 or 1')
 
 
 if __name__ == '__main__':
