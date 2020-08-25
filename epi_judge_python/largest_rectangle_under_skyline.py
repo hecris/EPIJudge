c46b8e9e647fe5a5846a26d382cf5f25f5c4ed3a
hecris <christopher.he33@myhunter.cuny.edu>
2020-08-25 12:34:32 -0400

    invariants: add suboptimal problem 17.8 (max rectangle under skyline)

--- epi_judge_python/largest_rectangle_under_skyline.py
+++ epi_judge_python/largest_rectangle_under_skyline.py
@@ -4,8 +4,18 @@ from test_framework import generic_test
 
 
 def calculate_largest_rectangle(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    # suboptimal divide and conquer solution
+    # worst case O(N^2)
+    def solution(lo, hi):
+        if lo > hi: return 0
+        if lo == hi: return heights[lo]
+
+        min_idx = min((i for i in range(lo, hi+1)), key= lambda idx: heights[idx])
+        left = solution(lo, min_idx-1)
+        right = solution(min_idx+1, hi)
+        return max(left, right, heights[min_idx] * (hi - lo + 1))
+
+    return solution(0, len(heights)-1)
 
 
 if __name__ == '__main__':
